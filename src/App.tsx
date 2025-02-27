264087c30fb75edbdc8ab0c08cc501633c7fef94
siriwatknp <siriwatkunaporn@gmail.com>
2025-02-27 18:33:55 +0700

    use StyledEngineProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { CacheProvider } from "@emotion/react";
 import {
   createTheme,
   ThemeProvider,
   useColorScheme,
+  StyledEngineProvider,
 } from "@mui/material/styles";
 import {
   Box,
@@ -11,37 +11,10 @@ import {
   TextField,
   Typography,
   IconButton,
+  GlobalStyles,
 } from "@mui/material";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import createCache, { StylisPlugin } from "@emotion/cache";
-
-const wrapInLayer: (layerName: string) => StylisPlugin =
-  (layerName) => (node) => {
-    // if we're not at the root of a <style> tag, leave the tree intact
-    // if the node is an @layer rule, leave it intact
-    if (node.root || node.type === "@layer") return;
-
-    // if we're at the root, replace node with `@layer layerName { node }`
-    const child = { ...node, parent: node, root: node };
-    Object.assign(node, {
-      children: [child],
-      length: 6,
-      parent: null,
-      props: [layerName],
-      return: "",
-      root: null,
-      type: "@layer",
-      value: `@layer ${layerName}`,
-    });
-  };
-
-const cache = createCache({
-  key: "css",
-  // By default, Tailwind has layer "@components" configured
-  // from the main CSS file, so this will work.
-  stylisPlugins: [wrapInLayer("components")],
-});
 
 function AppContent() {
   const { mode, setMode } = useColorScheme();
@@ -77,6 +50,7 @@ function AppContent() {
           </Typography>
           <Box component="form" sx={{ width: "100%" }}>
             <TextField
+              // @ts-expect-error ignore
               margin="normal"
               required
               fullWidth
@@ -84,9 +58,9 @@ function AppContent() {
               label="Email Address"
               name="email"
               autoComplete="email"
-              autoFocus
             />
             <TextField
+              // @ts-expect-error ignore
               margin="normal"
               required
               fullWidth
@@ -123,11 +97,12 @@ function App() {
   });
 
   return (
-    <CacheProvider value={cache}>
-      <ThemeProvider theme={theme}>
+    <StyledEngineProvider enableCssLayer injectFirst>
+      <GlobalStyles styles="@layer base, theme, components, mui, utilities;" />
+      <ThemeProvider theme={theme} noSsr defaultMode="system">
         <AppContent />
       </ThemeProvider>
-    </CacheProvider>
+    </StyledEngineProvider>
   );
 }
 
